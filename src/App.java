b0472fd289ce241d57051004e3543ffd8f3bc2e2
pedroleonez <147958375+pedroleonez@users.noreply.github.com>
2024-07-23 04:10:24 -0300

    Initial commit

---
+++ src/App.java
@@ -0,0 +1,11 @@
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        String nome = "maria";
+        int idade = 31;
+        double renda = 4000.0;
+        
+        System.out.printf("%s tem %d anos e ganha R$ %.2f reais.", nome, idade, renda);
+    }
+}
