515e9c6beb8a64eae41a275c8102c23313d287fe
pedroleonez <pedroleonez1@gmail.com>
2024-08-26 23:28:02 -0300

    iniciando exercício POO básico

---
+++ src/POO/BASICO/Estoque.java
@@ -0,0 +1,5 @@
+package POO.BASICO;
+
+public class Estoque {
+
+}
---
+++ src/POO/BASICO/Produto.java
@@ -0,0 +1,14 @@
+package POO.BASICO;
+
+import java.util.Objects;
+import java.util.Scanner;
+
+public class Produto {
+
+    public String nome;
+    public double preco;
+    public int estoque;
+
+
+
+}
