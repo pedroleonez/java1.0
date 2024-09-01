1fe7099173a9b3ad23399d83cf422f9d8e0bd130
pedroleonez <pedroleonez1@gmail.com>
2024-09-01 19:53:26 -0300

    Update Product.java

--- src/POO/BASICO/Product.java
+++ src/POO/BASICO/Product.java
@@ -39,6 +39,10 @@ public class Product {
         return quantity;
     }
 
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
     public double totalValueInStock() {
         return price * quantity;
     }
