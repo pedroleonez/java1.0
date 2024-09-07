8600edc8103e24564a85a4ab0feb082b6640fd7a
pedroleonez <pedroleonez1@gmail.com>
2024-09-06 22:37:01 -0300

    update Intro.java

--- src/POO/LISTAS/Intro.java
+++ src/POO/LISTAS/Intro.java
@@ -1,5 +1,48 @@
 package POO.LISTAS;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Intro {
-    
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+
+        list.add("Maria");
+        list.add("Pedro");
+        list.add("Jose");
+        list.add("Joao");
+        list.add(2, "Marco");
+
+        System.out.println(list.size());
+
+        for (String l : list) {
+            System.out.println(l);
+        }
+
+        System.out.println("------------------------");
+
+        list.removeIf(x -> x.charAt(0) == 'J');
+
+        for (String l : list) {
+            System.out.println(l);
+        }
+
+        System.out.println("------------------------");
+
+        System.out.println("Index of Pedro = " + list.indexOf("Pedro"));
+
+        System.out.println("------------------------");
+
+        List<String> result = list.stream().filter(x -> x.charAt(0) == 'M').toList();
+
+        for (String l : result) {
+            System.out.println(l);
+        }
+
+        System.out.println("------------------------");
+
+        String name = list.stream().filter(x -> x.charAt(0) == 'M').findFirst().orElse(null);
+        System.out.println(name);
+    }
 }
