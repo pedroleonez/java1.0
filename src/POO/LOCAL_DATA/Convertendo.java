65659201b280b3b03d75ed86ee960a3e30a1987b
pedroleonez <pedroleonez1@gmail.com>
2024-09-09 21:26:32 -0300

    update LOCAL_DATA Convertendo

--- src/POO/LOCAL_DATA/Convertendo.java
+++ src/POO/LOCAL_DATA/Convertendo.java
@@ -1,7 +1,20 @@
 package POO.LOCAL_DATA;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Convertendo {
     public static void main(String[] args) {
-        
+        LocalDate d04 = LocalDate.parse("2022-07-20");
+        LocalDateTime d05 = LocalDateTime.parse("2022-07-20T01:30:26");
+        Instant d06 = Instant.parse("2022-07-20T01:30:41Z");
+
+        DateTimeFormatter fmt1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+        System.out.println("d04 = " + d04.format(fmt1));
+        System.out.println("d04 = " + fmt1.format(d04));
+        System.out.println("d04 = " + d04.format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
     }
 }
