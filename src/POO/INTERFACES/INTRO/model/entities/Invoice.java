71ecce739ba12627dcdfdb769a8f24493fe64b1a
pedroleonez <147958375+pedroleonez@users.noreply.github.com>
2024-10-21 00:31:57 -0300

    iniciando INTERFACES

---
+++ src/POO/INTERFACES/INTRO/model/entities/Invoice.java
@@ -0,0 +1,35 @@
+package POO.INTERFACES.INTRO.model.entities;
+
+public class Invoice {
+
+    private Double basicPayment;
+    private Double tax;
+
+    private Invoice() {
+    }
+
+    public Invoice(Double basicPayment, Double tax) {
+        this.basicPayment = basicPayment;
+        this.tax = tax;
+    }
+
+    public Double getBasicPayment() {
+        return basicPayment;
+    }
+
+    public void setBasicPayment(Double basicPayment) {
+        this.basicPayment = basicPayment;
+    }
+
+    public Double getTax() {
+        return tax;
+    }
+
+    public void setTax(Double tax) {
+        this.tax = tax;
+    }
+
+    public Double getTotalPayment() {
+        return getBasicPayment() - getTax();
+    }
+}
