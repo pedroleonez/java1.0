cfe569ff46a94b57bb9ecfc34c3d4cb31783d122
pedroleonez <147958375+pedroleonez@users.noreply.github.com>
2024-10-22 22:34:50 -0300

    update INTERFACES

--- src/POO/INTERFACES/INTRO/application/Program.java
+++ src/POO/INTERFACES/INTRO/application/Program.java
@@ -1,5 +1,10 @@
 package POO.INTERFACES.INTRO.application;
 
+import POO.INTERFACES.INTRO.model.entities.CarRental;
+import POO.INTERFACES.INTRO.model.entities.Vehicle;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -9,6 +14,17 @@ public class Program {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
+        DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm", Locale.US);
+
+        System.out.println("Entre com os dados do aluguel:");
+        System.out.print("Modelo do carro: ");
+        String carModel = sc.nextLine();
+        System.out.println("Retirada (dd/MM/yyyy hh:mm): ");
+        LocalDateTime start = LocalDateTime.parse(sc.nextLine(), fmt);
+        System.out.println("Retorno (dd/MM/yyyy hh:mm): ");
+        LocalDateTime finish = LocalDateTime.parse(sc.nextLine(), fmt);
+
+        CarRental cr = new CarRental(start, finish, new Vehicle(carModel));
 
 
 
